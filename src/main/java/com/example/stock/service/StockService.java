12825fae2fae8d1d1e2e1fbc783eb53a590759ad
hshan <hhsung0120@naver.com>
2023-04-09 23:59:42 +0900

    2023-04-09 * 재고 감소 동시성 처리 * Synchronized 이용해보기

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,6 +4,7 @@ import com.example.stock.entity.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -15,7 +16,22 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
+    @Transactional
     public void decrease(Long id, Long quantity) {
+        // get stock
+        // 재고감소
+        // 저장
+
+        Stock stock = stockRepository.findById(id).orElseThrow();
+        stock.decrease(quantity);
+
+        stockRepository.save(stock);
+    }
+
+    //synchronized 사용해도 동시성 처리 실패
+    //@Transactional 동작 구조 때문에 실패, 주석해야 성공
+    //하지만 이렇게 되면 서버가 여러개 일때는 문제가 발생한다!
+    public synchronized void decrease2(Long id, Long quantity) {
         // get stock
         // 재고감소
         // 저장
--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -49,7 +49,7 @@ class StockServiceTest {
     void 동시에_100개_요청() throws InterruptedException {
         int threadCount = 100;
 
-        //비동기 서비스 자바API
+        //비동기로 실행하는 자바 API
         ExecutorService executorService = Executors.newFixedThreadPool(32);
 
         //다른스레드에서 작업이 완료할때까지 기다려주도록 도와주는 클래스
@@ -59,6 +59,34 @@ class StockServiceTest {
             executorService.submit(() -> {
                 try {
                     stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        //레이스 컨디션 발생? 두개이상의 스레드에서 같은 값을 변경할때 생기는 현상
+        assertThat(stock.getQuantity()).isEqualTo(0L);
+    }
+
+    @Test
+    void 동시에_100개_요청2() throws InterruptedException {
+        int threadCount = 100;
+
+        //비동기로 실행하는 자바 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        //다른스레드에서 작업이 완료할때까지 기다려주도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease2(1L, 1L);
                 } finally {
                     latch.countDown();
                 }
